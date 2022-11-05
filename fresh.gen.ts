4111d6a97967dc6fb1af95cc05a75e91efcdb93d
singcl <1276837689@qq.com>
2022-11-05 19:25:59 +0800

    feat(page): copy home page.

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -7,13 +7,14 @@ import * as $0 from "./routes/_404.tsx";
 import * as $1 from "./routes/_500.tsx";
 import * as $2 from "./routes/_middleware.ts";
 import * as $3 from "./routes/docs/[...slug].tsx";
-import * as $4 from "./routes/gfm.css.ts";
-import * as $5 from "./routes/index.tsx";
-import * as $6 from "./routes/raw.ts";
-import * as $7 from "./routes/showcase.tsx";
-import * as $8 from "./routes/update.tsx";
-import * as $9 from "./routes/xmvideo/_middleware.ts";
-import * as $10 from "./routes/xmvideo/update/[platform]/[version].tsx";
+import * as $4 from "./routes/fresh.tsx";
+import * as $5 from "./routes/gfm.css.ts";
+import * as $6 from "./routes/index.tsx";
+import * as $7 from "./routes/raw.ts";
+import * as $8 from "./routes/showcase.tsx";
+import * as $9 from "./routes/update.tsx";
+import * as $10 from "./routes/xmvideo/_middleware.ts";
+import * as $11 from "./routes/xmvideo/update/[platform]/[version].tsx";
 import * as $$0 from "./islands/CopyArea.tsx";
 import * as $$1 from "./islands/Counter.tsx";
 import * as $$2 from "./islands/LemonDrop.tsx";
@@ -24,13 +25,14 @@ const manifest = {
     "./routes/_500.tsx": $1,
     "./routes/_middleware.ts": $2,
     "./routes/docs/[...slug].tsx": $3,
-    "./routes/gfm.css.ts": $4,
-    "./routes/index.tsx": $5,
-    "./routes/raw.ts": $6,
-    "./routes/showcase.tsx": $7,
-    "./routes/update.tsx": $8,
-    "./routes/xmvideo/_middleware.ts": $9,
-    "./routes/xmvideo/update/[platform]/[version].tsx": $10,
+    "./routes/fresh.tsx": $4,
+    "./routes/gfm.css.ts": $5,
+    "./routes/index.tsx": $6,
+    "./routes/raw.ts": $7,
+    "./routes/showcase.tsx": $8,
+    "./routes/update.tsx": $9,
+    "./routes/xmvideo/_middleware.ts": $10,
+    "./routes/xmvideo/update/[platform]/[version].tsx": $11,
   },
   islands: {
     "./islands/CopyArea.tsx": $$0,

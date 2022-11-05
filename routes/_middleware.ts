8147c24cee8107b9badbbfdeb4a5d82907e62150
singcl <1276837689@qq.com>
2022-11-05 16:34:24 +0800

    feat(core): json ctx

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -1,7 +1,8 @@
+import type { MiddlewareHandlerContext } from "$fresh/server.ts";
 import resJson from "xmvideoplayer/utils/resJson.ts";
 export async function handler(
   _req: Request,
-  ctx: MiddlewareHandlerContextX<unknown>
+  ctx: MiddlewareHandlerContext<unknown>
 ) {
   Object.defineProperty(ctx, "json", {
     value: resJson,
--- typings/global.d.ts
+++ typings/global.d.ts
@@ -1,13 +1,13 @@
 import type {
-  MiddlewareHandlerContext,
+  // MiddlewareHandlerContext,
   HandlerContext,
 } from "$fresh/server.ts";
 
 declare global {
-  interface MiddlewareHandlerContextX<State = Record<string, unknown>>
-    extends MiddlewareHandlerContext<State> {
-    json?(data: unknown, statusCode: number): Response;
-  }
+  // interface MiddlewareHandlerContextX<State = Record<string, unknown>>
+  //   extends MiddlewareHandlerContext<State> {
+  //   json?(data: unknown, statusCode: number): Response;
+  // }
 
   interface HandlerContextX<Data = unknown, State = Record<string, unknown>>
     extends HandlerContext<Data, State> {

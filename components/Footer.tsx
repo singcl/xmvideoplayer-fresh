7e5bad49a790164df91798f8c171374d47a88a03
singcl <1276837689@qq.com>
2022-11-16 15:57:38 +0800

    feat: update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -15,16 +15,16 @@ const LINKS = [
 
 export default function Footer() {
   return (
-    <footer class="border(t-2 gray-200) bg-gray-100 h-32 flex flex-col gap-4 justify-center">
-      <div class="mx-auto max-w-screen-lg flex items-center justify-center gap-8">
+    <footer class="border(t-2 gray-100) bg-gray-100 h-20 flex flex-col gap-4 justify-center">
+      {/* <div class="mx-auto max-w-screen-lg flex items-center justify-center gap-8">
         {LINKS.map((link) => (
           <a href={link.href} class="text-gray-600 hover:underline">
             {link.title}
           </a>
         ))}
-      </div>
+      </div> */}
       <div class="text(gray-600 center)">
-        <span>© {new Date().getFullYear()} the fresh authors</span>
+        <span>© {new Date().getFullYear()} the XmVideoPlayer authors</span>
       </div>
     </footer>
   );
